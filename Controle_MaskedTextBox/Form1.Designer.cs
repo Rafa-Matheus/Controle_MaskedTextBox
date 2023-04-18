11779710c4310bae765a70af126a7417c451d40f
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-04-18 16:15:56 -0300

    Added example showing how to work with Text Masks

---
+++ Controle_MaskedTextBox/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Controle_MaskedTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox1_Numeros = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2_Letras = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox3_Datas = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox4_PlacaDeCarro = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox5_Hora = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox6_Decimais = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox7_Senha = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox8_Celular = new System.Windows.Forms.MaskedTextBox();
+            this.button_Verificar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1_Numeros
+            // 
+            this.maskedTextBox1_Numeros.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maskedTextBox1_Numeros.Location = new System.Drawing.Point(336, 111);
+            this.maskedTextBox1_Numeros.Name = "maskedTextBox1_Numeros";
+            this.maskedTextBox1_Numeros.Size = new System.Drawing.Size(125, 27);
+            this.maskedTextBox1_Numeros.TabIndex = 0;
+            // 
+            // maskedTextBox2_Letras
+            // 
+            this.maskedTextBox2_Letras.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maskedTextBox2_Letras.Location = new System.Drawing.Point(336, 145);
+            this.maskedTextBox2_Letras.Name = "maskedTextBox2_Letras";
+            this.maskedTextBox2_Letras.Size = new System.Drawing.Size(125, 27);
+            this.maskedTextBox2_Letras.TabIndex = 1;
+            // 
+            // maskedTextBox3_Datas
+            // 
+            this.maskedTextBox3_Datas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maskedTextBox3_Datas.Location = new System.Drawing.Point(336, 179);
+            this.maskedTextBox3_Datas.Name = "maskedTextBox3_Datas";
+            this.maskedTextBox3_Datas.Size = new System.Drawing.Size(125, 27);
+            this.maskedTextBox3_Datas.TabIndex = 2;
+            // 
+            // maskedTextBox4_PlacaDeCarro
+            // 
+            this.maskedTextBox4_PlacaDeCarro.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maskedTextBox4_PlacaDeCarro.Location = new System.Drawing.Point(336, 213);
+            this.maskedTextBox4_PlacaDeCarro.Name = "maskedTextBox4_PlacaDeCarro";
+            this.maskedTextBox4_PlacaDeCarro.Size = new System.Drawing.Size(125, 27);
+            this.maskedTextBox4_PlacaDeCarro.TabIndex = 3;
+            // 
+            // maskedTextBox5_Hora
+            // 
+            this.maskedTextBox5_Hora.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maskedTextBox5_Hora.Location = new System.Drawing.Point(336, 247);
+            this.maskedTextBox5_Hora.Name = "maskedTextBox5_Hora";
+            this.maskedTextBox5_Hora.Size = new System.Drawing.Size(125, 27);
+            this.maskedTextBox5_Hora.TabIndex = 4;
+            // 
+            // maskedTextBox6_Decimais
+            // 
+            this.maskedTextBox6_Decimais.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maskedTextBox6_Decimais.Location = new System.Drawing.Point(336, 280);
+            this.maskedTextBox6_Decimais.Name = "maskedTextBox6_Decimais";
+            this.maskedTextBox6_Decimais.Size = new System.Drawing.Size(125, 27);
+            this.maskedTextBox6_Decimais.TabIndex = 5;
+            // 
+            // maskedTextBox7_Senha
+            // 
+            this.maskedTextBox7_Senha.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maskedTextBox7_Senha.Location = new System.Drawing.Point(336, 314);
+            this.maskedTextBox7_Senha.Name = "maskedTextBox7_Senha";
+            this.maskedTextBox7_Senha.Size = new System.Drawing.Size(125, 27);
+            this.maskedTextBox7_Senha.TabIndex = 6;
+            // 
+            // maskedTextBox8_Celular
+            // 
+            this.maskedTextBox8_Celular.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maskedTextBox8_Celular.Location = new System.Drawing.Point(336, 348);
+            this.maskedTextBox8_Celular.Name = "maskedTextBox8_Celular";
+            this.maskedTextBox8_Celular.Size = new System.Drawing.Size(125, 27);
+            this.maskedTextBox8_Celular.TabIndex = 7;
+            // 
+            // button_Verificar
+            // 
+            this.button_Verificar.Location = new System.Drawing.Point(336, 391);
+            this.button_Verificar.Name = "button_Verificar";
+            this.button_Verificar.Size = new System.Drawing.Size(125, 29);
+            this.button_Verificar.TabIndex = 8;
+            this.button_Verificar.Text = "Verificar";
+            this.button_Verificar.UseVisualStyleBackColor = true;
+            this.button_Verificar.Click += new System.EventHandler(this.button_Verificar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(164, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 31);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Demonstrando o uso de máscaras de texto:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Verificar);
+            this.Controls.Add(this.maskedTextBox8_Celular);
+            this.Controls.Add(this.maskedTextBox7_Senha);
+            this.Controls.Add(this.maskedTextBox6_Decimais);
+            this.Controls.Add(this.maskedTextBox5_Hora);
+            this.Controls.Add(this.maskedTextBox4_PlacaDeCarro);
+            this.Controls.Add(this.maskedTextBox3_Datas);
+            this.Controls.Add(this.maskedTextBox2_Letras);
+            this.Controls.Add(this.maskedTextBox1_Numeros);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1_Numeros;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2_Letras;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3_Datas;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4_PlacaDeCarro;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox5_Hora;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox6_Decimais;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox7_Senha;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox8_Celular;
+        private System.Windows.Forms.Button button_Verificar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Controle_MaskedTextBox/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controle_MaskedTextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            /* 
+             * Para números são usados os seguintes caracteres:
+             * 0 -> Para números OBRIGATÓRIOS
+             * # -> Para números OPCIONAIS
+            */
+            maskedTextBox1_Numeros.Mask = "0000"; // 4 números obrigatórios 
+            // maskedTextBox1_Numeros.Mask = "####"; // 4 números opcionais
+            // maskedTextBox1_Numeros.Mask = "0#"; // 1 obrigatório e 1 opcional
+
+
+            /*
+             * Para letras são usados os caracteres:
+             * L -> Para letras OBRIGATÓRIAS
+             * T -> Para letras OPCIONAIS
+            */
+            maskedTextBox2_Letras.Mask = "LLLLLL"; // 6 letras obrigatórias
+                                                   // maskedTextBox2_Letras.Mask = "TTTTTT"; // 6 letras opcionais
+                                                   // maskedTextBox2_Letras.Mask = "LLTT"; // 2 obrigatórias e 2 opcionais
+
+
+            /*
+             * Para datas:
+            */
+            maskedTextBox3_Datas.Mask = "00/00/0000";
+
+
+            /*
+             * Para placas veículares:
+            */
+            maskedTextBox4_PlacaDeCarro.Mask = "LLL - 0000";
+
+
+            /*
+             * Para horas: 
+            */
+            maskedTextBox5_Hora.Mask = "00:00:00";
+
+
+            /*
+             * Para decimais:
+            */
+            maskedTextBox6_Decimais.Mask = "00.0000"; // O marcador de decial é o "."
+
+
+            /*
+             * Para senhas:
+            */
+            maskedTextBox7_Senha.Mask = "0000";
+            maskedTextBox7_Senha.PasswordChar = '*'; // Substitui os nºs por "*"
+
+
+            /*
+             * Para celular:
+            */
+            maskedTextBox8_Celular.Mask = "(00) \\90000-0000";
+            // "(\\000) \\90000-0000" | obs.: "\\" força o caracter seguinte
+        }
+
+        private void button_Verificar_Click(object sender, EventArgs e)
+        {
+            bool todosPreenchidos = this.Controls.OfType<MaskedTextBox>().All(mtb => mtb.MaskCompleted);
+
+            if (todosPreenchidos) MessageBox.Show("Formulário totalmente preenchido");
+            else MessageBox.Show("Preencha " + this.Name +" corretamente.");
+        }
+    }
+}
